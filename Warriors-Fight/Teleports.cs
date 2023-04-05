ad64f9b38e5aff7bf26c54ad415716d0d3eec442
Maycon Jardim <mayconsmill@gmail.com>
2023-04-04 23:53:17 -0300

    Classes auxiliares para teleporte do lutador

---
+++ Warriors-Fight/Teleports.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Warriors_Fight
+{
+     interface Teleports
+    {
+        string teleport();
+    }
+}
